1b6367d7caa5f4692f7f7e0aeee49e03cf84a9fe
hexis <hexis@pop-os.localdomain>
2022-10-09 20:43:20 -0400

    1.0

--- install.sh
+++ install.sh
@@ -1,9 +1,58 @@
 #!/bin/bash
 
+prompt() {
+	read e
+
+	echo "--------------------------------------"
+	echo "- do you want to install hexOS (y/n) -"
+        echo "--------------------------------------"
+
+	if [ "$e" == "y" ]; then
+		setup
+	fi
+
+	if [ "$e" == "n" ]; then
+		exit 1
+	fi
+}
+
+
 
 setup() {
+	echo "--------------"
+	echo "- installing -"
+	echo "--------------"
+        
+	sleep 1
+	mkdir ~/.xmonad && mv ~/hexOS/xmonad/xmonad.hs ~/.xmonad && mkdir ~/.config/xmobar && mv ~/hexOS/xmobar/xmobarrc ~/.config/xmobar
 
-	mkdir ~/.xmonad && mv ~/hexOS/
+
+}
+
+
+
+
+
+config() {
+	echo "---------------------"
+	echo "- configuring hexOS -"
+	echo "---------------------"
+	
+	
+	echo -n "would you like to replace your .bashrc with the hexOS .bashrc (y/n)"
+        read -r y/n
+
+	if [ "y/n" == "y" ]; then
+		sleep 1
+		mv ~/hexOS/bash-config/.bashrc ~/ && mv ~/hexOS/compton/compton.conf ~/.config/ && 
+	fi
+
+	if [ "y/n" == "n" ]; then
+		
+
+
+
+
 
 
 
